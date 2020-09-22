0fc5ec38711e19e8c9563a32b9fab6b8fe0a80d5
mkashaev <kashaevmt@gmail.com>
2020-09-22 21:24:23 +0300

    ADD Mob scene

--- Player.gd
+++ Player.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+signal hit
 export var speed = 400
 var screen_size
 
@@ -21,3 +22,25 @@ func _process(delta):
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
+
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
+	
+func _on_Player_body_entered(body):
+	hide()
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
